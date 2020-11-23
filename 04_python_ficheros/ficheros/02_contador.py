5a94a48ffab1cc2f62f2aa8316a73ff7fd77c148
Ivan <ivancoellocortes@gmail.com>
2020-11-23 12:45:58 +0100

    commit on dev branch, an example has been modified

--- 04_python_ficheros/ficheros/02_contador.py
+++ 04_python_ficheros/ficheros/02_contador.py
@@ -6,24 +6,18 @@ if __name__ == '__main__':
     fichero = open("utilidades/monitorContador.txt", "a+")
     fichero.seek(0)
     contenido = fichero.readline()
-
     if len(contenido) == 0:
         contenido = "0"
         fichero.write(contenido)
-
     fichero.close()
-
     try:
         contador = int(contenido)
-
         if len(sys.argv) == 2:
-            if sys.argv[1] == "+":
+            if sys.argv[1] == "inc":
                 contador += 1
-            elif sys.argv[1] == "-":
+            elif sys.argv[1] == "dec":
                 contador -= 1
-
         print(contador)
-
         fichero = open("utilidades/monitorContador.txt", "w")
         fichero.write(str(contador))
         fichero.close()
