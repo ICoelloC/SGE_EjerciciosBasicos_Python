cd8ee950afe5b0942b26b959b08e3fba1ca9de7f
ICoelloC <ivancoellocortes@gmail.com>
2020-12-15 17:43:24 +0100

    commit on dev branch, added new examaples to the repository

---
+++ 08_POO/generador_contrasena/generarPassMain.py
@@ -0,0 +1,45 @@
+from password import Password
+
+
+def numPassword():
+    valorcorrecto = False
+    npassword = 0
+    while not valorcorrecto:
+        try:
+            npassword = input("Introduce el numeor de passwords que qioeres generar: ")
+            npassword = int(npassword)
+            valorcorrecto = True
+        except ValueError:
+            print("La longitud debe ser un entrero")
+    return npassword
+
+
+def generarListaPassword():
+    npassword = numPassword()
+
+    listapassword = []
+    long = -1
+    for i in range(npassword):
+        msg = "Longitud de la contraseña {0} (-1 para longitud por defecto): "
+        valorcorrecto = False
+        while not valorcorrecto:
+            long = input(msg.format(str(i)))
+            try:
+                long = int(long)
+                valorcorrecto = True
+            except ValueError:
+                print("La longitud debe ser un entero")
+        if long == -1:
+            listapassword.append(Password())
+        else:
+            listapassword.append(Password(long))
+    return listapassword
+
+
+def mostrarListaPassword(listapassword):
+    for p in listapassword:
+        print(p)
+
+
+listapassword = generarListaPassword()
+mostrarListaPassword(listapassword)
---
+++ 08_POO/generador_contrasena/password.py
@@ -0,0 +1,58 @@
+from random import choice
+
+class Password():
+    def __init__(self, long=8):
+        self.longitud = long
+        self.generarPass()
+
+    @property
+    def longitud(self):
+        return self.__longitud
+
+    @longitud.setter
+    def longitud(self, long):
+        if long > 0:
+            self.__longitud = long
+        else:
+            self.__longitud = 8
+            raise ValueError("La longitud introducida tiene que ser mayor que cero")
+
+    @property
+    def valor(self):
+        return self.__valor
+
+    def __str__(self):
+        clase = type(self).__name__
+        msg = "{0} de longitud {1} y valor {2}"
+        if self.esFuerte():
+            msg += " es fuerte"
+        else:
+            msg += " es débil"
+        return msg.format(clase, self.longitud, self.valor)
+
+    def generarPass(self):
+        valores = "0123456789abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ<>=@#%&+"
+        self.__valor = ""
+        for i in range(self.longitud):
+            self.__valor += choice(valores)
+
+    def esFuerte(self):
+        """Será fuerte si tiene más 2 mayúsculas, más 1 minúscula y más de 5 números"""
+        if len(self.__valor) < 8:
+            return False
+        else:
+            mins = "abcdefghijklmnñopqrstuvwxyz"
+            mays = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+            num = "0123456789"
+            nmins, nmays, nnums = 0, 0, 0
+            for a in self.__valor:
+                if a in mins:
+                    nmins += 1
+                elif a in mays:
+                    nmays += 1
+                elif a in num:
+                    nnums += 1
+            if nmays > 2 and nmins > 1 and nnums > 5:
+                return True
+            else:
+                return False
