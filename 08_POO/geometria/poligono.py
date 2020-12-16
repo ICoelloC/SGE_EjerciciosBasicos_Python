8a58a10b8a27ac218366c3a7d8368fbb733dd84d
ICoelloC <ivancoellocortes@gmail.com>
2020-12-16 10:44:59 +0100

    modified examples

--- 08_POO/geometria/poligono.py
+++ 08_POO/geometria/poligono.py
@@ -1,7 +1,7 @@
-class Poligono:
+class Poligono():
     def __init__(self, *args):
         self.lados = args
-        self.__nlados = len(self.lados)
+        self.__nlados=len(self.lados)
 
     @property
     def lados(self):
@@ -15,6 +15,7 @@ class Poligono:
     def nlados(self):
         return self.__nlados
 
+
     def __str__(self):
         clase = type(self).__name__
         msg = "{0} de {1} lados con longitudes {2}"
@@ -23,3 +24,4 @@ class Poligono:
     def getPerimetro(self):
         """Devuelve el perímetro del polígono"""
         return sum(self.lados)
+
